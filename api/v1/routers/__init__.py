386739888d91f066b483399cc4e07b81aaf4d211
John Willman <johnpwillman@gmail.com>
2022-10-12 15:44:44 -0500

    Remove v1/ route and update route descriptions

--- api/v1/routers/__init__.py
+++ api/v1/routers/__init__.py
@@ -6,18 +6,18 @@ router = APIRouter(
     tags=['v1']
 )
 
-@router.get("/", status_code=status.HTTP_200_OK)
-def index():
-    return {'message': 'Hello v1'}
-
 @router.get("/pokemon", status_code=status.HTTP_200_OK)
 def filter_pokemon(request: Request):
-    """Filter pokemon by path query"""
+    """
+    Filter pokemon by path query. (Query value need not match exactly)
+    e.g. ?name=char (Will return Charmander, Charmeleon, and Charizard entries)
+    """
     print(request.query_params)
     return pd.filter_pokedex(**request.query_params)
 
 @router.get("/pokemon/{id}", status_code=status.HTTP_200_OK)
 def pokemon_by_number(id: int):
+    """Return pokedex entry for pokemon whose number matches `id`"""
     try:
        return pd.filter_pokedex(num=str(id).rjust(3, '0'))
     except:
