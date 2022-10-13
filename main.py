abe9ecdd05f779ffc772b498747bc47406707ef9
John Willman <johnpwillman@gmail.com>
2022-10-12 22:34:20 -0500

    Configure fastapi to serve output of npm run build

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from api.v1 import routers as v1_router
 
 app = FastAPI()
 
 app.include_router(v1_router.router)
+app.mount("/static", StaticFiles(directory="web/dist"), name="static")
+app.mount("/assets", StaticFiles(directory="web/dist/assets"), name="assets")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="web/dist")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
